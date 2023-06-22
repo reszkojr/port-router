f07d4fa55720035c2f2aaa46d4846817bb16acdf
Fabio Jr <s.reszkojr@gmail.com>
2023-06-22 12:13:59 -0300

    databases, instances and style improvements

--- router.py
+++ router.py
@@ -12,12 +12,34 @@ branches = [
     'ERP3',
 ]
 
+databases = [
+    'Stage',
+    'Helisul (pesado esse hein)',
+    'Azul',
+    'EPA',
+]
+
+instances = [
+    None, # erp1
+    None, # erp2
+    None, # erp3
+    None, # erp4
+]
+
 ip_hub = {}
+
 
 templates = Jinja2Templates(directory="templates")
 
 @app.get('/')
 def router(request: Request):
     ip = request.client.host
+    context = {
+        "request": request,
+        "ip": ip,
+        "branches": branches,
+        "dbs": databases,
+        "instances": instances
+        }
 
-    return templates.TemplateResponse("router.html", {"request": request, "ip": ip})
+    return templates.TemplateResponse("router.html", context)
