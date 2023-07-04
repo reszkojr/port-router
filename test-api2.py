d0276bb0518dc42be3f9c54e5248a626567a3503
reszkojr <s.reszkojr@gmail.com>
2023-07-04 04:12:52 +0000

    test apis

--- test-api2.py
+++ test-api2.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 import uvicorn
 
-app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get('/')
-def app():
-    return "app2"
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def api(request: Request):
+    return templates.TemplateResponse("api.html", {"request": request, "api": 2})
 
 if __name__ == '__main__':
-    uvicorn.run("test-apis:app", host="0.0.0.0", port=2, reload=True)
+    uvicorn.run("test-api2:app", host="0.0.0.0", port=2, reload=True)
--- test-api3.py
+++ test-api3.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 import uvicorn
 
-app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get('/')
-def app():
-    return "app3"
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def api(request: Request):
+    return templates.TemplateResponse("api.html", {"request": request, "api": 3})
 
 if __name__ == '__main__':
-    uvicorn.run("test-apis:app", host="0.0.0.0", port=3, reload=True)
+    uvicorn.run("test-api3:app", host="0.0.0.0", port=3, reload=True)
